1051e2a8e83dcccaaa674716f3bb44f7e6c217c9
ChloeArd <chloe.ardoise@gmail.com>
2020-11-23 13:41:08 +0100

    Exercice 51

---
+++ script.js
@@ -0,0 +1,12 @@
+let result = 5 + 6;
+
+let addition = result + 2;
+
+let soustraction = result - 1;
+
+alert(result * addition);
+
+alert(result ** soustraction)
+
+let modulo = 5 % 2;
+alert(modulo);
